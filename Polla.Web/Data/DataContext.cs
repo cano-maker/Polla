2780d129527e415329d8344dc28aa0ba47b07f5c
Fabio Cano <fabiocano894@gmail.com>
2022-03-27 16:45:45 -0500

    Add data base configurations

---
+++ Polla.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Polla.Web.Data.Entities;
+
+namespace Polla.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<TeamEntity> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TeamEntity>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
---
+++ Polla.Web/Data/Entities/TeamEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Polla.Web.Data.Entities
+{
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Name { get; set; }
+    }
+}
